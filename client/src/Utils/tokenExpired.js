3746ef48d7fa459e4f2a7493cd957c24d4a1ff01
Sania Singla <singlasania7717@gmail.com>
2025-04-17 18:56:35 +0530

    cleanup using wrappers & login redirects

--- client/src/Utils/tokenExpired.js
+++ client/src/Utils/tokenExpired.js
@@ -1,4 +1,4 @@
-export async function checkTokenExpired(res,) {
+export async function checkTokenExpired(res, setUser) {
     if (res && res.message === 'tokens missing') {
         setUser(null);
     }
