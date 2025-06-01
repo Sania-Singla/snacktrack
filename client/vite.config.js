71fd04d74fe584ad928fff7ae0c60add4790eef7
Sania Singla <singlasania7717@gmail.com>
2025-06-01 17:15:17 +0530

    frontend deployed on vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './src/Config/.env' });
 
 export default defineConfig(() => {
     return { plugins: [react(), tailwindcss()] };
