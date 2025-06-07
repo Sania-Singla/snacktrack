9bcea0ca5ce263a9a13ae7bfce9a2e3b297babe4
Sania Singla <singlasania7717@gmail.com>
2025-06-07 08:52:46 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,7 +36,7 @@ export default function App() {
     }, [location]);
 
     return (
-        <div className="bg-white h-dvw w-dvw">
+        <div className="bg-white h-lvw w-lvw">
             {loading ? (
                 <div className="text-black h-full w-full flex flex-col items-center justify-center">
                     <div className="size-[33px] fill-[#4977ec] dark:text-[#ececec]">
