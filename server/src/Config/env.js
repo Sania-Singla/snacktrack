c47accb997246467f82e903c6eb99a50750fb731
Sania-Singla <singlasania7717@gmail.com>
2025-10-06 17:30:39 +0530

    env changed

--- server/src/Config/env.js
+++ server/src/Config/env.js
@@ -4,6 +4,6 @@ import path from 'path';
 dotenv.config({
     path:
         process.env.ENV === 'production'
-            ? '/etc/.env'
+            ? '/etc/snacktrack_server.env'
             : path.resolve(process.cwd(), '.env.local'),
 });
